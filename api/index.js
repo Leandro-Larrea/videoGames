9feb136634bb52717f4b16828bc9dc56a9ccd0bd
unknown <meliodas_17@hotmail.com>
2022-12-16 01:17:08 -0300

    force true, start: node index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ require('dotenv').config();
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
