5ad239dce989cfe1417c6ce08f636bb2be267186
unknown <meliodas_17@hotmail.com>
2022-12-17 03:49:41 -0300

    volviendo a la forma original de db deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,31 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = process.env;
-let sequelize = new Sequelize(`postgres:uR3hY4fMtO5xN4DrO9jY@containers-us-west-100.railway.app:6850/railway`)
-  // process.env.NODE_ENV === "production"
-  //   ? 
-//     : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+const sequelize = process.env.NODE_ENV === "production"? new Sequelize({
+  database: DB_NAME,
+  dialect: "postgres",
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  pool: {
+    max: 3,
+    min: 1,
+    idle: 10000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    keepAlive: true,
+  },
+  ssl: true,
+}):
+  new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+   logging: false, 
+   native: false, 
+ });
 
 
 
