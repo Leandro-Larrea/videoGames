bcd3564a398c375f30c0e2702b2228afe1716b56
unknown <meliodas_17@hotmail.com>
2022-12-17 00:19:02 -0300

    json y commentar en db

--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,13 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = process.env;
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize(`postgres:uR3hY4fMtO5xN4DrO9jY@containers-us-west-100.railway.app:6850/railway`)
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+let sequelize = new Sequelize(`postgres:uR3hY4fMtO5xN4DrO9jY@containers-us-west-100.railway.app:6850/railway`)
+  // process.env.NODE_ENV === "production"
+  //   ? 
+//     : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 
 
 
