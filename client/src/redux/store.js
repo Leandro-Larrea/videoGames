331c778d6a33c21f5662412a7697d34f0009c50a
unknown <meliodas_17@hotmail.com>
2022-12-18 00:50:56 -0300

    store structure changed

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware, compose} from "redux";
 import {rootReducer} from "./reducer.js";
-import thunk from "redux-thunk";
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-)
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
